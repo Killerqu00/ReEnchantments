d81c761ed0f54902c4bdfd8130bda0c2e4d7c001
Killerqu00 <killerqueen1777@gmail.com>
2023-08-29 22:58:34 +0200

    init

---
+++ src/main/java/com/killerqu/reenchantments/ReEnchantments.java
@@ -0,0 +1,13 @@
+package com.killerqu.reenchantments;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReEnchantments implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/killerqu/reenchantments/client/ReEnchantmentsClient.java
@@ -0,0 +1,13 @@
+package com.killerqu.reenchantments.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReEnchantmentsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
