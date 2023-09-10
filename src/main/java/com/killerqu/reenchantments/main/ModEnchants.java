0196fa4b5e5555eab463d299480b154f7d5db104
Killerqu00 <killerqueen1777@gmail.com>
2023-09-10 12:18:56 +0200

    add HermesBlessingEnchant

--- src/main/java/com/killerqu/reenchantments/main/ModEnchants.java
+++ src/main/java/com/killerqu/reenchantments/main/ModEnchants.java
@@ -1,9 +1,6 @@
 package com.killerqu.reenchantments.main;
 
-import com.killerqu.reenchantments.enchantments.CrippleEnchant;
-import com.killerqu.reenchantments.enchantments.ExperiencedEnchant;
-import com.killerqu.reenchantments.enchantments.MidasTouchEnchant;
-import com.killerqu.reenchantments.enchantments.VampirismEnchant;
+import com.killerqu.reenchantments.enchantments.*;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,6 +12,7 @@ public class ModEnchants {
     public static final Enchantment VAMPIRISM = register("vampirism", new VampirismEnchant());
     public static final Enchantment MIDASTOUCH = register("midas_touch", new MidasTouchEnchant());
     public static final Enchantment CRIPPLE = register("cripple", new CrippleEnchant());
+    public static final Enchantment HERMESBLESSING = register("hermes_blessing", new HermesBlessingEnchant());
 
     private static Enchantment register(String name, Enchantment enchantment) {
         return Registry.register(Registry.ENCHANTMENT, new Identifier(ReEnchantments.MOD_ID, name), enchantment);
