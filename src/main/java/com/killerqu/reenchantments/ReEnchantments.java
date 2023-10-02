64522433a61e8010b2f3269e51f04588b5873b6e
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-10-02 15:15:46 +0100

    code overhaul
    down to 1 class, 1 mixin and 1 interface

--- src/main/java/com/killerqu/reenchantments/ReEnchantments.java
+++ src/main/java/com/killerqu/reenchantments/ReEnchantments.java
@@ -1,17 +1,20 @@
-package com.killerqu.reenchantments.main;
+package com.killerqu.reenchantments;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReEnchantments implements ModInitializer {
     public static final String MOD_ID = "reenchantments";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    /**
-     * Runs the mod initializer.
-     */
+
     @Override
     public void onInitialize() {
-        ModEnchants.registerAll(LOGGER);
+        ModEnchants.init();
+    }
+
+    public static Identifier id(String name) {
+        return new Identifier(MOD_ID, name);
     }
 }
